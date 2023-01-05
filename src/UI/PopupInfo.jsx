ecf9f5f8144e4c1536244438b2806b79a7abdc8a
Daniel Ugarov <tyradire@yandex.ru>
2023-01-05 13:15:55 +0300

    feat: add copy func to popupinfo, minor style fixes

--- src/UI/PopupInfo.jsx
+++ src/UI/PopupInfo.jsx
@@ -4,6 +4,10 @@ import './PopupInfo.scss';
 
 const PopupInfo = ({ popupOpened, title, ingridients, data }) => {
 
+  const copyIngridient = (e) => {
+    navigator.clipboard.writeText(e.currentTarget.querySelector('.popup-info__unit').textContent.split('-')[0]);
+  }
+
   return (
     <div className={popupOpened ? 'popup-info popup-info_opened' : 'popup-info'}>
       <p className='popup-info__title'>Ingridients for {title}</p>
@@ -11,9 +15,12 @@ const PopupInfo = ({ popupOpened, title, ingridients, data }) => {
         {
           Object.keys(ingridients[0]).length && ingridients.map((elem, i) => {
             const img = data.filter(el => el.title === elem.unit)[0].img;
-            return <li className='popup-info__list-item' key={i}>
+            return <li className='popup-info__list-item' 
+              key={i}
+              onClick={copyIngridient}
+            >
               <img className='popup-info__image' src={img} alt={elem} width='30' height='30' />
-              <p className='popup-info__unit'>{elem.unit} &ndash; {elem.amount}</p>
+              <p className='popup-info__unit'>{elem.unit} - {elem.amount}</p>
               <div className='popup-info__copy'>
                 <CopyIcon />
               </div>
